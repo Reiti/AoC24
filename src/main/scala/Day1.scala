643264bbc952dedb65e9a7bccaaaf1f443777d2d
Michael Reitgruber <reitgruber.michael@gmail.com>
2024-12-02 18:13:20 +0100

    xd

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -1,11 +1,11 @@
-import util.Day;
+import util.Day
 
 object Day1 extends Day(1):
   override def solve(): Unit =
     val (l, r) = inputLines.map(_.split(" {3}")).map(r => (r(0).toInt, r(1).toInt)).unzip
 
     //Part 1
-    println(l.sorted.zip(r.sorted).map((l, r) => Math.abs(l - r)).sum)
+    println(l.sorted.zip(r.sorted).map((l, r) => (l - r).abs).sum)
 
     val counts = r.map(i => i -> r.count(_ == i)).toMap.withDefaultValue(0)
 
--- src/main/scala/Day2.scala
+++ src/main/scala/Day2.scala
@@ -10,8 +10,8 @@ object Day2 extends Day(2):
     //Part 2
     println(input.map(withTolerance).map(l => l.find(safe)).count(_.isDefined))
 
-  def safe(seq: Array[Int]): Boolean = safe(seq.sliding(2).map(l => (l.head, l(1))).toArray, (l, r) => Math.signum(l - r) == Math.signum(seq.head - seq(1)))
+  def safe(seq: Array[Int]): Boolean = safe(seq.sliding(2).map(l => (l.head, l(1))).toArray, (l, r) => (l - r).sign == (seq.head - seq(1)).sign)
 
-  def safe(seq: Array[(Int, Int)], step: (Int, Int) => Boolean): Boolean = seq.forall((l, r) => step(l, r) && Math.abs(l - r) >= 1 && Math.abs(l - r) <= 3)
+  def safe(seq: Array[(Int, Int)], step: (Int, Int) => Boolean): Boolean = seq.forall((l, r) => step(l, r) && (l - r).abs >= 1 && (l - r).abs <= 3)
 
   def withTolerance(seq: Array[Int]): List[Array[Int]] = seq.indices.map(i => seq.patch(i, Nil, 1)).toList
