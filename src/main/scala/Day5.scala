c669add717d248b822618eff6faf3b14c5180d36
Michael Reitgruber <Reiti@users.noreply.github.com>
2024-12-05 13:21:08 +0100

    Day 5

--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -3,24 +3,19 @@ import util.Day
 object Day5 extends Day(5):
   override def solve(): Unit =
     val split = input.split("\n\n")
-    val ordering = split.head.split("\n")
+    val orderingRules = split.head.split("\n")
     val updates = split(1).split("\n").map(u => u.split(","))
-    val orderMap = ordering.map(o => o.split("\\|")).groupBy(_.head).view.mapValues(_.map(_(1)).toSet).toMap
+    val orderMap = orderingRules.map(o => o.split("\\|")).groupBy(_.head).view.mapValues(_.map(_(1)).toSet).toMap.withDefaultValue(Set())
+
+    given Ordering[String] with
+      def compare(x: String, y: String): Int = if orderMap(y).contains(x) then 1 else -1
 
     //Part 1
-    println(updates.filter(a => a.sliding(2).forall(l => before(l.head, l(1), orderMap))).map(l => l(l.length/2).toInt).sum)
+    println(updates.filter(inOrder).map(l => l(l.length/2).toInt).sum)
 
-    val incorrectlyOrdered = updates.filter(a => !a.sliding(2).forall(l => before(l.head, l(1), orderMap)))
+    val incorrectlyOrdered = updates.filter(a => !inOrder(a))
 
     //Part 2
-    println(incorrectlyOrdered.map(u => u.sortWith((l, r) => before(l, r, orderMap))).map(l => l(l.length/2).toInt).sum)
+    println(incorrectlyOrdered.map(u => u.sorted).map(l => l(l.length/2).toInt).sum)
 
-  def before(a: String, b: String, orderMap: Map[String, Set[String]]): Boolean =
-    if !orderMap.contains(a) then
-      false
-    else if orderMap.contains(b) && orderMap(b).contains(a) then
-      false
-    else if orderMap(a).contains(b) then
-      true
-    else
-      orderMap(a).exists(nb => before(b, nb, orderMap))
+  def inOrder(a: Array[String])(using ord: Ordering[String]): Boolean = a.sliding(2).forall(l => ord.lteq(l.head, l(1)))
