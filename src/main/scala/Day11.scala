9c1214020b011d6d657e9a477d543c92f6d2ef2f
Michael Reitgruber <Reiti@users.noreply.github.com>
2024-12-11 13:34:18 +0100

    Day 11

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -10,9 +10,8 @@ object Day11 extends Day(11):
     //Part 2
     println(stones.map(s => countStones(s, 75)).sum)
 
-  lazy val countStones: ((String, Int)) => Long = Util.memoize {
+  lazy val countStones: ((String, Int)) => Long = Util.memoize:
     case (_, 0) => 1
     case ("0", blinks) => countStones("1", blinks - 1)
     case (num, blinks) if num.length % 2 == 0 => num.grouped(num.length / 2).map(s => countStones(s.toLong.toString, blinks - 1)).sum
     case (num, blinks) => countStones((num.toLong * 2024L).toString, blinks - 1)
-  }
