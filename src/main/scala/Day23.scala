6e249a0ac6b7de8de49926246382316e9ef4fb50
Michael Reitgruber <reitgruber.michael@gmail.com>
2024-12-23 11:13:13 +0100

    Day 23

--- src/main/scala/Day23.scala
+++ src/main/scala/Day23.scala
@@ -1,5 +1,7 @@
 import util.Day
 
+import scala.annotation.tailrec
+
 object Day23 extends Day(23):
   override def solve(): Unit =
     val conns = inputLines.map(_.split("-")).flatMap{case Array(a, b) => List((a, b), (b, a))}.groupBy(_._1).view.mapValues(_.map(_._2).toSet).toMap
@@ -8,18 +10,32 @@ object Day23 extends Day(23):
     println(find3Cliques(conns).count(l => l.exists(s => s.startsWith("t"))))
 
     //Part 2
-    println(conns.keys.map(k => maxClique(conns, Set(k))).maxBy(_.size).toList.sorted.mkString(","))
+    println(maxClique(conns, conns.keySet.map(k => Set(k))).toList.sorted.mkString(","))
 
   def find3Cliques(conns: Map[String, Set[String]]): Set[Set[String]] =
     conns.keys.flatMap(s =>
       conns(s).toList.combinations(2).map(_.toList).filter{ case List(a, b) => conns(a).contains(b) && conns(b).contains(a) }.map(c => Set(c.head, c(1), s))
     ).toSet
 
-  def maxClique(conns: Map[String, Set[String]], acc: Set[String]): Set[String] =
-    conns.keys.foldLeft(acc){
-      case (connected, next) =>
-        if connected.forall(c => conns(c).contains(next) && conns(next).contains(c)) then
-          connected + next
-        else
-          connected
-    }
+  @tailrec
+  def maxClique(conns: Map[String, Set[String]], cliques: Set[Set[String]]): Set[String] =
+    val next = cliques.flatMap(c => grow(conns, c))
+
+    if next.isEmpty then
+      cliques.head
+    else
+      maxClique(conns, next)
+
+  def grow(conns: Map[String, Set[String]], clique: Set[String]): Set[Set[String]] =
+    val ext = conns.keySet.filter(k => clique.forall(v => conns(v).contains(k) && conns(k).contains(v)))
+    ext.map(e => clique + e)
+
+  //Incorrect (only finds a maximal clique, not maximum) but works for the input
+//  def maxClique(conns: Map[String, Set[String]], acc: Set[String]): Set[String] =
+//    conns.keys.foldLeft(acc){
+//      case (connected, next) =>
+//        if connected.forall(c => conns(c).contains(next) && conns(next).contains(c)) then
+//          connected + next
+//        else
+//          connected
+//    }
