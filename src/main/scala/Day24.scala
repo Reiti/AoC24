63b754ef2372cc7a2ab5874fdf220725625508b8
Michael Reitgruber <reitgruber.michael@gmail.com>
2024-12-24 14:22:25 +0100

    Day 24

--- src/main/scala/Day24.scala
+++ src/main/scala/Day24.scala
@@ -13,8 +13,31 @@ object Day24 extends Day(24):
     //Part 1
     println(BigInt(num, 2))
 
-    //Part 2 (found by hand)
-    println(List("cbd","gmh","jmq","qrh","rqf","z06","z13","z38").mkString(","))
+    //Part 2
+    println((1 to 44).flatMap(g => findErrors(gates, g)).distinct.sorted.mkString(","))
+
+  def findErrors(gates: List[(String, String)], idx: Int): List[String] =
+    val fi = "%02d".format(idx)
+    val gate = "z" + fi
+    val found = gates.find(_._2 == gate).get
+
+    if !found._1.contains("XOR") then
+      val needed1 = ("x" + fi) + " XOR " + ("y" + fi)
+      val needed2 = ("y" + fi) + " XOR " + ("x" + fi)
+      val corr = gates.find(g => g._1 == needed1 || g._1 == needed2).get._2
+      val swap = gates.find(g => g._1.contains("XOR " + corr) || g._1.contains(corr + " XOR")).get
+      List(found._2, swap._2)
+    else
+      val comp = found._1.split(" XOR ")
+      val c1 = gates.find(_._2 == comp.head).get
+      val c2 = gates.find(_._2 == comp(1)).get
+
+      if !c1._1.contains("XOR") && !c2._1.contains("XOR") then
+        val nc = gates.find(g => g._1.contains("XOR") && g._1.contains("x"+fi)).get
+        val sw = if c1._1.contains(fi) then c1 else c2
+        List(nc._2, sw._2)
+      else
+        List.empty
 
   @tailrec
   def calculate(values: Map[String, String], gates: List[(String, String)]): Map[String, String] =
